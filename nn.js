e77ce30b9b83dadf9c77f53bbcf5ecbb4594e8a6
Phillip Dodd <pdodd@uga.edu>
2019-08-05 16:13:39 -0400

    why doesn't subtract work? line 83

--- nn.js
+++ nn.js
@@ -19,7 +19,7 @@ class NeuralNetwork {
         //* weights between input and hidden layers
         this.weights_ih = new Matrix(this.input_nodes, this.hidden_nodes);
         this.weights_ih.randomize();
-        
+
         //* weights between hidden and output layers
         this.weights_ho = new Matrix(this.hidden_nodes, this.output_nodes);
         this.weights_ho.randomize();
@@ -40,7 +40,7 @@ class NeuralNetwork {
         //* Generate Hidden Outputs
         let hidden = Matrix.multiply(this.weights_ih, input);
         hidden.add(this.bias_h);
-        
+
         //* Pass through activation function
         hidden.map(sigmoid);
         //* Generate the Output's Output
@@ -54,35 +54,55 @@ class NeuralNetwork {
     train(input_array, target_array) {
         // let outputs = this.feedforward(inputs);
         let input = Matrix.fromArray(input_array);
+
         //* Generate Hidden Outputs
         let hidden = Matrix.multiply(this.weights_ih, input);
         hidden.add(this.bias_h);
 
         //* Pass through activation function
         hidden.map(sigmoid);
+        console.log('hidden')
+        console.table(hidden.data)
 
         //* Generate the Output's Output
         let outputs = Matrix.multiply(this.weights_ho, hidden);
         outputs.add(this.bias_o);
+        
+        //* Pass through activation function
         outputs.map(sigmoid);
+        console.log('outputs')
+        console.table(outputs.data)
 
         //* Convert array to matrix object
         let targets = Matrix.fromArray(target_array)
 
         //* Calculate the error
         // ERROR = TARGETS - OUTPUTS
+        console.log(`targets.rows: ${targets.rows} outputs.rows: ${outputs.rows}`);
+
         let output_errors = Matrix.subtract(targets, outputs);
+        console.log('output_errors')
+        console.table(output_errors.data)
 
         //* Calculate gradient
         let gradients = Matrix.map(outputs, dsigmoid);
         gradients.multiply(output_errors);
         gradients.multiply(this.learningRate);
+        console.log('gradients')
+        console.table(gradients.data)
 
+        
         //* Calculate hidden -> output deltas
         let hidden_T = Matrix.transpose(hidden);
         let weights_ho_deltas = Matrix.multiply(gradients, hidden_T);
         this.weights_ho.add(weights_ho_deltas);
 
+        //* Adjust the weights by the deltas
+        this.weights_ho.add(weights_ho_deltas);
+
+        //* Adjust the bias by its deltas (which are just the gradients)
+        this.bias_o.add(gradients);
+        
         // TODO create a loop to handle the multiple hidden layers
         //* Calculate the hidden layer errors
         let transposed_weights_ho = Matrix.transpose(this.weights_ho);
@@ -91,12 +111,17 @@ class NeuralNetwork {
         //* Calculate hidden gradient
         let hidden_gradient = Matrix.map(hidden, dsigmoid);
         hidden_gradient.multiply(hidden_errors);
-        hidden_gradient.multiply(this.learningRate);  
-        
+        hidden_gradient.multiply(this.learningRate);
+
         //* Calculate input -> hidden deltas
         let inputs_T = Matrix.transpose(input);
         let weights_ih_deltas = Matrix.multiply(hidden_gradient, inputs_T);
+
+        //* Adjust the weights of the deltas
         this.weights_ih.add(weights_ih_deltas);
+
+        //* Adjust the bias by its deltas (which are just the gradients)
+        this.bias_h.add(gradients);
 
         outputs.print();
         targets.print();
--- sketch.js
+++ sketch.js
@@ -1,9 +1,34 @@
+console.clear()
+let training_data = [
+    {
+        inputs: [1, 0],
+        targets: [1]
+    },
+    {
+        inputs: [0, 1],
+        targets: [1]
+    },
+    {
+        inputs: [1, 1],
+        targets: [0]
+    },
+    {
+        inputs: [0, 0],
+        targets: [0]
+    }
+];
+
 function setup() {
     let nn = new NeuralNetwork(2, 2, 2);
-    let inputs = [1, 0];
-    let targets = [1, 0];
-    nn.train(inputs, targets);
+    for (var i = 0; i < 50000; i++) {
+        let data = random(training_data);
+        nn.train(data.inputs, data.targets);
+    }
 
+    console.log(nn.feedforward([0, 1]))
+    console.log(nn.feedforward([1, 0]))
+    console.log(nn.feedforward([1, 1]))
+    console.log(nn.feedforward([0, 0]))
 }
 
 function draw() {
